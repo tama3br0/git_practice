09f14bb53c6fd3b871507b8cf81414dd63141209
tama3br0 <newairport180@gmail.com>
2023-11-03 04:05:43 +0900

    暗号化追加

--- password_maneger.sh
+++ password_maneger.sh
@@ -2,6 +2,8 @@
 
 # パスワードデータを保存するファイル
 password_file="passwords.txt"
+# 暗号化されたファイル
+encrypted_file="passwords.gpg"
 
 # パスワードを保存
 save_password() {
@@ -11,24 +13,29 @@ save_password() {
     read -s -p "パスワード: " password
     echo ""
     
-    # 既存の情報があるか確認
-    existing_data=$(grep "$service" "$password_file")
-    if [ -n "$existing_data" ]; then
-        # 既存の情報を削除
-        sed -i "/$service/d" "$password_file"
-    fi
-    
-    echo "パスワードの追加は成功しました。"
-    echo "次の選択肢から入力してください (Add Password/Get Password/Change Password/Exit):"
-    echo ""
+    # パスワード情報をファイルに保存
     echo "$service $username $password" >> "$password_file"
+    
+    # パスワードファイルを暗号化
+    gpg --output "$encrypted_file" --encrypt --recipient your-email@example.com "$password_file"
+    
+    echo "パスワードの追加と暗号化は成功しました."
+    echo "次の選択肢から入力してください (Add Password/Get Password/Exit):"
 }
 
 # パスワードを取得
 get_password() {
     echo "サービス名を入力してください："
     read -p "サービス名: " service
+    
+    # 暗号化されたファイルを復号化
+    gpg --output "$password_file" --decrypt "$encrypted_file"
+    
     data=$(grep "$service" "$password_file")
+    
+    # 復号化後、パスワードファイルを削除
+    rm "$password_file"
+    
     if [ -n "$data" ]; then
         username=$(echo "$data" | awk '{print $2}')
         password=$(echo "$data" | awk '{print $3}')
@@ -40,44 +47,13 @@ get_password() {
         echo ""
         echo "そのサービスは登録されていません。"
     fi
-    echo "次の選択肢から入力してください (Add Password/Get Password/Change Password/Exit):"
-}
-
-# パスワードを変更
-change_password() {
-    echo "サービス名を入力してください："
-    read -p "サービス名: " service
-    data=$(grep "$service" "$password_file")
-    if [ -n "$data" ]; then
-        echo "現在の情報："
-        username=$(echo "$data" | awk '{print $2}')
-        password=$(echo "$data" | awk '{print $3}')
-        echo "サービス名：$service"
-        echo "ユーザー名：$username"
-        echo "パスワード：$password"
-        echo ""
-        
-        read -p "新しいユーザー名: " new_username
-        read -s -p "新しいパスワード: " new_password
-        echo ""
-        
-        # 既存の情報を削除
-        sed -i "/$service/d" "$password_file"
-        
-        echo "パスワードの変更は成功しました。"
-        echo "次の選択肢から入力してください (Add Password/Get Password/Change Password/Exit):"
-        echo ""
-        echo "$service $new_username $new_password" >> "$password_file"
-    else
-        echo ""
-        echo "そのサービスは登録されていません。"
-    fi
-    echo "次の選択肢から入力してください (Add Password/Get Password/Change Password/Exit):"
+    
+    echo "次の選択肢から入力してください (Add Password/Get Password/Exit):"
 }
 
 # ウェルカムメッセージを表示
 echo "パスワードマネージャーへようこそ！"
-echo "次の選択肢から入力してください (Add Password/Get Password/Change Password/Exit):"
+echo "次の選択肢から入力してください (Add Password/Get Password/Exit):"
 
 while true; do
     read -p "選択してください: " choice
@@ -89,15 +65,12 @@ while true; do
         "Get Password")
             get_password
             ;;
-        "Change Password")
-            change_password
-            ;;
         "Exit")
             echo "Thank you!"
             exit 0
             ;;
         *)
-            echo "入力が間違えています。Add Password/Get Password/Change Password/Exit から入力してください."
+            echo "入力が間違えています。Add Password/Get Password/Exit から入力してください."
             ;;
     esac
 done
